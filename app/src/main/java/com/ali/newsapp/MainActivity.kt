91fe16cf847f4161143a896e12a97efbccc52c74
Mohsin0899 <mohsin.saifi98@gmail.com>
2023-02-04 23:34:54 +0530

    Final Commit

--- app/src/main/java/com/ali/newsapp/MainActivity.kt
+++ app/src/main/java/com/ali/newsapp/MainActivity.kt
@@ -3,30 +3,44 @@ package com.ali.newsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: NewsAdapter
+    private var articles = mutableListOf<Article>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = NewsAdapter(this@MainActivity, articles)
+        val newsList = findViewById<RecyclerView>(R.id.newsList)
+        newsList.adapter = adapter
+        newsList.layoutManager = LinearLayoutManager(this@MainActivity)
         getNews()
     }
 
     private fun getNews() {
         val news = NewsService.newsInstance.getHeadlines("in", 1)
         news.enqueue(object : Callback<News> {
+            override fun onFailure(call: Call<News>, t: Throwable) {
+                Log.d("Ali", "Error in Fetching News", t)
+            }
+
             override fun onResponse(call: Call<News>, response: Response<News>) {
                 val news = response.body()
                 if (news != null) {
                     Log.d("Ali", news.toString())
+                    articles.addAll(news.articles)
+                    adapter.notifyDataSetChanged()
                 }
             }
 
-            override fun onFailure(call: Call<News>, t: Throwable) {
-                Log.d("Ali", "Error in Fetching News", t)
-            }
+
         })
     }
 }
