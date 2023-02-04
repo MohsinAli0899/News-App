6d778a641dcbe89d431e81e9adcf55dc15acc3ad
Mohsin0899 <mohsin.saifi98@gmail.com>
2023-02-04 13:49:00 +0530

    Implemented Retrofit

---
+++ app/src/main/java/com/ali/newsapp/Article.kt
@@ -0,0 +1,3 @@
+package com.ali.newsapp
+
+data class Article(val author: String, val title: String, val description: String, val url: String, val urlToImage: String)
---
+++ app/src/main/java/com/ali/newsapp/News.kt
@@ -0,0 +1,3 @@
+package com.ali.newsapp
+
+data class News(val totalResults: Int, val articles: List<Article>)
